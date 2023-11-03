a8a4847d473ac27e342f1c0237f4a0de2acd1a7d
fitsaleem <sherohero33@gmail.com>
2023-11-03 07:39:08 -0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,30 @@ export default function RootLayout({ children }) {
     <>
       <html lang="en">
         <head>
+        <title>
+        FitCodding | Master Web3 Development, Blockchain, and Cloud Computing
+        </title>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="google-site-verification" content="nUs1sZlixkttFP_uk2dtojO4an6Aw90hyysa7U08hk8" />
           
           {/* Add the keywords meta tag here */}
+          <meta property="og:url" content="https://www.fitcodding.com/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="FitCodding | Master Web3 Development, Blockchain, and Cloud Computing"/>
+        <meta property="og:description" content="FitCodding is your ultimate resource for mastering web development, computer science, and coding. Dive into the world of blockchain, explore web3 development, and unlock the potential of cloud computing with our comprehensive guides and tutorials."/>
+        <meta property="og:image" content="https://cdn.builder.io/api/v1/image/assets%2F167f9ae46d92433593fe8462bf97f4f3%2F6fe77d4677cf427aba5d7085d2f75cab"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="fitcodding.com"/>
+        <meta property="twitter:url" content="https://www.fitcodding.com" />
+        <meta name="twitter:title" content="FitCodding | Master Web3 Development, Blockchain, and Cloud Computing"/>
+        <meta name="twitter:description" content="FitCodding is your ultimate resource for mastering web development, computer science, and coding. Dive into the world of blockchain, explore web3 development, and unlock the potential of cloud computing with our comprehensive guides and tutorials." />
+        <meta name="twitter:image" content="https://cdn.builder.io/api/v1/image/assets%2F167f9ae46d92433593fe8462bf97f4f3%2F6fe77d4677cf427aba5d7085d2f75cab"/>
+          <meta
+          name="description"
+          content="FitCodding is your ultimate resource for mastering web development, computer science, and coding. Dive into the world of blockchain, explore web3 development, and unlock the potential of cloud computing with our comprehensive guides and tutorials."
+        />
           <meta name="keywords" content="Web Development, Computer Science, Coding Tutorials, Blockchain Technology, Web3 Development, Cloud Computing, JavaScript Programming, Next.js Tutorials, Blockchain Development, Cloud Services, Web Development Guides, Computer Science Lessons, Coding for Beginners, Advanced Coding Techniques, Blockchain for Beginners, Web3 Applications, Cloud Infrastructure, Full Stack Development, Frontend Development, Backend Development" />
 
           <title>{metadata.title}</title>
--- src/app/page.js
+++ src/app/page.js
@@ -5,30 +5,41 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
-import { Toaster } from 'react-hot-toast';
-
+import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Toaster 
-         position="top-center" 
-         reverseOrder={true}
-         containerStyle={{
-          top:150, 
-           // Adjust this value to position it horizontally.
-         }}
-       
-      />
-      <Navbar />
-      <div class="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>
+          FitCodding | Master Web3 Development, Blockchain, and Cloud Computing
+        </title>
+        <meta
+          name="description"
+          content="FitCodding is your ultimate resource for mastering web development, computer science, and coding. Dive into the world of blockchain, explore web3 development, and unlock the potential of cloud computing with our comprehensive guides and tutorials."
+        />
+          <meta name="keywords" content="Web Development, Computer Science, Coding Tutorials, Blockchain Technology, Web3 Development, Cloud Computing, JavaScript Programming, Next.js Tutorials, Blockchain Development, Cloud Services, Web Development Guides, Computer Science Lessons, Coding for Beginners, Advanced Coding Techniques, Blockchain for Beginners, Web3 Applications, Cloud Infrastructure, Full Stack Development, Frontend Development, Backend Development" />
+      </Head>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Toaster
+          position="top-center"
+          reverseOrder={true}
+          containerStyle={{
+            top: 150,
+            // Adjust this value to position it horizontally.
+          }}
+        />
+        <Navbar />
+        <div class="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
