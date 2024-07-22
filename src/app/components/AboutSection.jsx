69634eb362245137154463c1517ac9331804b7da
fitsaleem <sherohero33@gmail.com>
2024-07-22 13:22:53 +0500

    update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -52,46 +52,44 @@ const AboutSection = () => {
 
   return (
     <section className="text-white mt-6" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/hero-image2.png" width={500} height={500} alt="A descriptive alt text"/>
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack Web3 developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript,TypeScript, React, Redux, Next.js, MongoDB, MYSQL,
-            , HTML, CSS, CI/CD , Doker and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
-        </div>
-      </div>
+<div className="grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+  <Image src="/images/hero-image2.png" width={500} height={500} alt="A descriptive alt text" className="order-2 md:order-1"/>
+  <div className="mt-4 md:mt-0 text-left flex flex-col h-full order-1 md:order-2">
+    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+    <p className="text-base lg:text-lg">
+      I am a full stack Web3 developer with a passion for creating
+      interactive and responsive web applications. I have experience
+      working with JavaScript, TypeScript, React, Redux, Next.js, MongoDB, MYSQL,
+      HTML, CSS, CI/CD, Docker, and Git. I am a quick learner and I am always
+      looking to expand my knowledge and skill set. I am a team player and
+      I am excited to work with others to create amazing applications.
+    </p>
+    <div className="flex flex-row justify-start mt-8">
+      <TabButton
+        selectTab={() => handleTabChange("skills")}
+        active={tab === "skills"}
+      >
+        Skills
+      </TabButton>
+      <TabButton
+        selectTab={() => handleTabChange("education")}
+        active={tab === "education"}
+      >
+        Education
+      </TabButton>
+      <TabButton
+        selectTab={() => handleTabChange("certifications")}
+        active={tab === "certifications"}
+      >
+        Certifications
+      </TabButton>
+    </div>
+    <div className="mt-8">
+      {TAB_DATA.find((t) => t.id === tab).content}
+    </div>
+  </div>
+</div>
+
     </section>
   );
 };
